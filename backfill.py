77661e745659ea52e0dfca7c556fecda2624a758
Caleb Burton <ccburton4@gmail.com>
2023-03-15 13:55:04 -0400

    feat: use env vars in backfill script

--- backfill.py
+++ backfill.py
@@ -1,23 +1,42 @@
+import os
 import pandas as pd
 from influxdb import InfluxDBClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Access environment variables from .env
+BACKFILL_INFLUX_HOST=os.getenv('BACKFILL_INFLUX_HOST')
+BACKFILL_INFLUX_USER=os.getenv('BACKFILL_INFLUX_USER')
+BACKFILL_INFLUX_PASS=os.getenv('BACKFILL_INFLUX_PASS')
+BACKFILL_FILEPATH=os.getenv('BACKFILL_FILEPATH')
+
+if (not BACKFILL_INFLUX_HOST or
+        not BACKFILL_INFLUX_USER or
+        not BACKFILL_INFLUX_PASS or
+        not BACKFILL_FILEPATH):
+    raise Exception('Missing environment variables')
 
 client = InfluxDBClient(
-            host='1.1.1.1',
+            host=BACKFILL_INFLUX_HOST,
             port=8086,
-            username='', # config/influxdb/influxdb.env --> DOCKER_INFLUXDB_INIT_USERNAME
-            password='', # config/influxdb/influxdb.env --> DOCKER_INFLUXDB_INIT_PASSWORD
+            username=BACKFILL_INFLUX_USER,
+            password=BACKFILL_INFLUX_PASS,
             database='home_assistant',
             ssl=False,
             verify_ssl=False
         )
 
-file_path = r'/Users/user/Downloads/states.csv'
+file_path = BACKFILL_FILEPATH
 
 csvReader = pd.read_csv(file_path)
 
 print(f'Input shape (rows, columns): {csvReader.shape}')
 print(f'Input column names: {csvReader.columns}')
 
+print(f'=============================================')
+print(f'Row Index\tTimestamp\t\tState')
+print(f'=============================================')
 for row_index, row in csvReader.iterrows() :
     timestamp = pd.to_datetime(row[0], unit='s')
     state = row[1]
@@ -40,9 +59,10 @@ for row_index, row in csvReader.iterrows() :
             "value": state,
         }
     }]
-    client.write_points(json_body)
-    # break
-# result = client.query('select * from "°F" WHERE time > 1676840000000000000 limit 1;')
+    # client.write_points(json_body)
+    break
+
+# result = client.query('select * from "°F" WHERE time > 1677715200000000000 AND time < 1678888800000000000 limit 1;')
 # print(f"Result: {result}")
 
 # Some helpful influx commands:
